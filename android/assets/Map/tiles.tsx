439fe8cb8d75677d657a3e911fb8a8fbd8a83632
Niklas Nson <niklasnson@me.com>
2017-10-04 05:56:17 +0200

    adding a new level.

--- android/assets/Map/tiles.tsx
+++ android/assets/Map/tiles.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tiles" tilewidth="32" tileheight="32" tilecount="16" columns="4">
  <image source="tiles.png" width="128" height="128"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="0"/>
+ </terraintypes>
 </tileset>
