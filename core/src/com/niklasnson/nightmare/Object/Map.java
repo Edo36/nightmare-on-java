449bde51ba1c1ca76e1e63d2c4c118f42c7fc968
Niklas Nson <niklasnson@me.com>
2017-10-04 05:57:05 +0200

    cleaning code

--- core/src/com/niklasnson/nightmare/Object/Map.java
+++ core/src/com/niklasnson/nightmare/Object/Map.java
@@ -28,7 +28,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
